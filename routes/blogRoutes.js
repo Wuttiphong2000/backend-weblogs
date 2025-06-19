15361fbffdc6cfea76694669c4543ee6e099319c
Wuttiphong2000 <wuttiphong.santhalunai@gmail.com>
2025-06-19 22:33:18 +0700

    First commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { addBlog, addComment, deleteBlogById, getAllBlogs, getBlogById, getBlogComment, togglePublished } from "../controllers/blogController.js";
+import upload from "../middleware/multer.js";
+import auth from "../middleware/auth.js";
+
+const blogRouter = express.Router();
+
+blogRouter.post("/add", upload.single("image"), auth, addBlog);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/delete",auth, deleteBlogById);
+blogRouter.post("/toggle-publish",auth, togglePublished);
+blogRouter.post("/add-comment", addComment);
+blogRouter.post("/comment", getBlogComment);
+
+export default blogRouter;
---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import  adminRouter  from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+
+const app = express();
+
+await connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.use("/api/admin", adminRouter);
+app.use("/api/blog", blogRouter);
+app.get("/", (req, res) => res.send("API is Working"));
+
+app.listen(PORT, () => {
+  console.log("Server is running on port" + PORT);
+});
+
+export default app;
